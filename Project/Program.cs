cd17ce3ef5cfd973a5e5cfd6cd923bd487cc675c
Artem Protsenko <temaprgod8@gmail.com>
2023-02-07 18:02:18 +0200

    Add digit separator

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,68 +1,101 @@
-﻿namespace Project
+﻿using System.Globalization;
+namespace Project
 {
     public class Program
     {
         static void Main()
         {
+            NumberFormatInfo newnum = new NumberFormatInfo{
+                NumberDecimalSeparator = ".",
+            };
             string output, answer;
-            Int64 num_1 = 0, num_2 = 0;
+            double num_1 = 0, num_2 = 0;
             bool again = false;
             do
             {
+                
                 Console.WriteLine("Hello and welcome to my first poop program :) can you please write a first number");
-                num_1 = Convert.ToInt64(Console.ReadLine());
+                num_1 = Convert.ToDouble(Convert.ToDecimal(Console.ReadLine(),newnum));
                 Console.WriteLine("And second :)");
-                num_2 = Convert.ToInt64(Console.ReadLine());
+                num_2 = Convert.ToDouble(Convert.ToDecimal(Console.ReadLine(),newnum));
                 Console.WriteLine("Write an operation between numbers For example: -, +, *, /, ^,fib1");
                 output = Console.ReadLine().ToLower();
-                
-            
-            switch(output){
-                case "+":
-                Console.WriteLine($"The sum of {num_1} and {num_2} = {num_1 + num_2}");
-                break;
-                case "-":
-                Console.WriteLine($"The difference of {num_1} and {num_2} = {num_1 - num_2}");
-                break;
-                case "*":
-                Console.WriteLine ($"The multiplication of {num_1} and {num_2} = {num_1 * num_2}");
-                break;
-                case "/":
-                Console.WriteLine($"The division of {num_1} and {num_2} = {(double) num_1 / num_2}");
-                break;
-                case "^":
-                Console.WriteLine($"Number {num_1} in degree {num_2} = {Math.Pow(num_1, num_2)}");
-                break;
-                default:
-                Console.WriteLine("wrong operation");
-                break;
-            }
+
+
+                switch (output)
+                {
+                    case "+":
+                        Console.WriteLine($"The sum of {num_1} and {num_2} = {num_1 + num_2}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"The difference of {num_1} and {num_2} = {num_1 - num_2}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"The multiplication of {num_1} and {num_2} = {num_1 * num_2}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"The division of {num_1} and {num_2} = {(double)num_1 / num_2}");
+                        break;
+                    case "^":
+                        Console.WriteLine($"Number {num_1} in degree {num_2} = {Math.Pow(num_1, num_2)}");
+                        break;
+                    default:
+                        Console.WriteLine("wrong operation");
+                        break;
+                }
                 bool AA = true;
-            do{
-                Console.WriteLine("If you want to use this application type \"Y\" into console, otherwise type \"N\" ");
-                answer = Console.ReadLine();
-                if (answer == "Y")
+                do
                 {
-                    again = true;
-                    AA = false;
-                    Console.Clear();
-                }
-                else if (answer == "N")
-                {
-                    again = false;
-                    AA = false;
-                }
-                else{
-                    Console.WriteLine("wrong operation try again");
-                    AA = true;
-                    
-                }
-            }while(AA);
+                    Console.WriteLine("If you want to use this application type \"Y\" into console, otherwise type \"N\" ");
+                    answer = Console.ReadLine();
+                    if (answer == "Y")
+                    {
+                        again = true;
+                        AA = false;
+                        Console.Clear();
+                    }
+                    else if (answer == "N")
+                    {
+                        again = false;
+                        AA = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("wrong operation try again");
+                        AA = true;
+
+                    }
+                } while (AA);
             } while (again);
             Console.WriteLine("Thats all");
             Console.WriteLine("If you practice more, you'll be the best");
             Console.ReadKey();
 
         }
+        // public static int find_indx(int[] seq)
+        // {
+        //     byte score = 0;
+        //     for (int i = 0; i < seq.Length; i++)
+        //     {
+        //         for (int j = 0; j < seq.Length; j++)
+        //         {
+        //             if (seq[i] == seq[j])
+        //             {
+        //                 score++;
+        //                 if (score % 2 != 0)
+        //                 {
+        //                     return score;
+        //                 }
+        //             }
+        //             else
+        //             {
+        //                 continue;
+        //             }
+        //         }
+
+        //     }
+
+        // }
     }
+
 }
